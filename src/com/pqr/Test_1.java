ec326945d47044efefe58b6c3359935dee14daf5
GovindPatil75 <mulegr75@gmail.com>
2022-02-20 17:21:55 +0530

    First Commit

--- src/com/pqr/Test_1.java
+++ src/com/pqr/Test_1.java
@@ -1,10 +1,14 @@
 package com.pqr;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("1");
+       System.out.println("1");
+       WebDriver driver=new ChromeDriver();
 	}
 
 }
